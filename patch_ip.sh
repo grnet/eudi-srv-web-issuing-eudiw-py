519a4676cf1c78f5a0fef249ca34cdf755c5f799
Marios Fragkoulis <mfg@grnet.gr>
2024-09-23 11:24:41 +0300

    Extend patch script for MACOS compatibility

--- patch_ip.sh
+++ patch_ip.sh
@@ -4,7 +4,15 @@ if [ "$1" == "" ]; then
     exit 1
 fi
 
+sed="sed"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	sed="sed"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	# brew install gnu-sed
+	sed="gsed"
+fi
+
 echo "Using IP: $1"
 for f in server_ip.conf app/app_config/config_service.py app/app_config/oid_config.json app/metadata_config/metadata_config.json app/metadata_config/openid-configuration.json run-issuer.sh; do
-    sed -i "s/192.168.134.214/$1/g" $f
+    $sed -i "s/192.168.134.214/$1/g" $f
 done
