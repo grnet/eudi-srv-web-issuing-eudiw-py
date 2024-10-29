843a98d91b82193387870938aef2d74cd1cc58f2
none <fmerg@protonmail.com>
2024-10-29 13:15:15 +0200

    Improve credential related methods

--- test_ssi.py
+++ test_ssi.py
@@ -1,34 +1,24 @@
 import os
 import json
+import re
+import jwt
 
 from ssi_lib import SSI as _SSI
-from ssi_lib import SSIIssuanceError, SSIGenerationError
+from ssi_lib import SSIIssuanceError, SSIGenerationError, SSIVerificationError
 
 class SSI(_SSI):
 
-    def _issue_vc(
-        self, keypath, holder_did, issuer_did, outfile
-    ):
+    def _issue_credential(self, keypath, holder_did, issuer_did, outfile):
+        # TODO: Improve script usage
         res, code = self._run_cmd([
-            'issue-credential',
-            '--key', keypath,
-            '--issuer', issuer_did,
-            '--holder', holder_did,
-            '--export',
-            '/home/dev/app/claims.json',
+            "issue-credential",
+            "--key", keypath,
+            "--issuer", issuer_did,
+            "--holder", holder_did,
+            "--export", outfile,
         ])
         return res, code
 
-    def issue_credential(
-        self, keypath, holder_did, issuer_did, outfile
-    ):
-        res, code = self._issue_vc(
-            keypath, holder_did, issuer_did, outfile)
-        if not code == 0:
-            raise SSIIssuanceError(res)
-        with open(outfile, 'r') as f:
-            credential = json.load(f)
-        return credential
 
     def _create_presentation(
         self,
@@ -41,17 +31,47 @@ class SSI(_SSI):
         presentation_submission_output,
         nonce=None
     ):
+        # TODO: Improve script usage
         res, code = self._run_cmd([
-            'present-credentials',
-            '-hd', holder_did,
-            '-hk', holder_keypath,
-            '-vd', verifier_did,
-            '-vc', vcfile,
-            '-pd', presentation_definition,
-            '-vp', vpfile,
-            '-ps', presentation_submission_output,
+            "present-credentials",
+            "-hd", holder_did,
+            "-hk", holder_keypath,
+            "-vd", verifier_did,
+            "-vc", vcfile,
+            "-pd", presentation_definition,
+            "-vp", vpfile,
+            "-ps", presentation_submission_output,
         ])
         return res, code
+
+
+    def _verify_presentation(
+        self,
+        holder_did,
+        presentation_definition,
+        presentation_submission_output,
+        vpfile,
+    ):
+        res, code = self._run_cmd([
+            "waltid-cli", "vp", "verify",
+            "-hd", holder_did,
+            "-pd", presentation_definition,
+            "-ps", presentation_submission_output,
+            "-vp", vpfile,
+        ])
+        return res, code
+
+
+    def issue_credential(self, keypath, holder_did, issuer_did, outfile):
+        res, code = self._issue_credential(keypath, holder_did, issuer_did, outfile)
+        if not code == 0:
+            raise SSIIssuanceError(res)
+
+        with open(outfile, "r") as f:
+            credential = json.load(f)
+
+        return credential
+
 
     def create_presentation(
         self,
@@ -76,9 +96,12 @@ class SSI(_SSI):
         )
         if not code == 0:
             raise SSIGenerationError(res)
-        with open(vpfile, 'r') as f:
+
+        with open(vpfile, "r") as f:
             presentation = f.read()
+
         return presentation
+
 
     def verify_presentation(
         self,
@@ -87,28 +110,23 @@ class SSI(_SSI):
         presentation_submission_output,
         vpfile,
     ):
-        res, code = self._run_cmd([
-            'waltid-cli',
-            'vp',
-            'verify',
-            '-hd', holder_did,
-            '-pd', presentation_definition,
-            '-ps', presentation_submission_output,
-            '-vp', vpfile,
-        ])
-        # TODO: How to infer verification failure?
-        import re
-        pattern = r'Success\s*\(([^()]*)\)'
+        res, code = self._verify_presentation(
+            holder_did,
+            presentation_definition,
+            presentation_submission_output,
+            vpfile,
+        )
+        if not code == 0:
+            raise SSIVerificationError(res)
+
+        pattern = r"Success\s*\(([^()]*)\)"
         ans = res.split("\n")[0]
         matches = re.findall(pattern, ans)
-        import json
-        credential_1 = json.loads(matches[0])
-        credential_2 = json.loads(matches[2])
-        embedded_did = matches[1]
-        return credential_1, credential_2, embedded_did
+        presentation = json.loads(matches[0])
+        return presentation
 
 
-ssi = SSI('/home/user/tmp') # TODO: Remove auxiliary tmp file
+ssi = SSI("/home/user/tmp")
 
 
 storage = "/home/dev/storage"
@@ -130,7 +148,7 @@ verifier_did_id = verifier_did["content"]["id"]
 
 
 credential = ssi.issue_credential(
-    issuer_keypath, holder_did_id, issuer_did_id, "claims.json"
+    issuer_keypath, holder_did_id, issuer_did_id, "/home/dev/app/claims.json",
 )
 
 presentation = ssi.create_presentation(
@@ -143,28 +161,21 @@ presentation = ssi.create_presentation(
     "/home/dev/app/outputPresSub.json"
 )
 
-credential_1, credential_2, embedded_did = ssi.verify_presentation(
+presentation = ssi.verify_presentation(
     holder_did_id,
     "/home/dev/app/presDef.json",
     "/home/dev/app/outputPresSub.json",
     "/home/dev/app/outputVp.jwt",
 )
 
-token = credential_1["vp"]["verifiableCredential"][0]
-import jwt
-decoded_token = decoded_token = jwt.decode(
+assert presentation["aud"] == verifier_did_id
+assert presentation["iss"] == holder_did_id
+assert presentation["sub"] == holder_did_id
+
+token = presentation["vp"]["verifiableCredential"][0]    # could me more than one
+credential = decoded_token = jwt.decode(
     token, options={"verify_signature": False}
 )
 
-
-assert credential_1["aud"] == verifier_did_id
-assert credential_1["iss"] == holder_did_id
-assert credential_1["sub"] == holder_did_id
-
-assert credential_2 == decoded_token
-
-assert credential_2["iss"] == issuer_did_id
-assert credential_2["sub"] == holder_did_id
-
-
-import pdb; pdb.set_trace()
+assert credential["iss"] == issuer_did_id
+assert credential["sub"] == holder_did_id
