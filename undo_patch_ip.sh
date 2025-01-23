7cbdd10439160967c91c38f757f2c442841276ba
fmerg <fmerg@grnet.gr>
2025-01-23 15:45:14 +0200

    Remove patch sripts

--- undo_patch_ip.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-git restore app/app_config/config_service.py app/app_config/oid_config.json app/metadata_config/metadata_config.json app/metadata_config/openid-configuration.json run-issuer.sh
