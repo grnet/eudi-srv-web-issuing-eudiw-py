ebab236d8ead69a258606bdd931c66b0c1a0ab2e
fmerg <fmerg@grnet.gr>
2025-02-07 12:35:17 +0200

    Explicitly pipe resolved IP to .config.ip

--- resolve-ip.sh
+++ resolve-ip.sh
@@ -10,5 +10,4 @@ fi
 IP_NEW=$(ip -o addr show dev "$interface" |
     while read IFNUM IFNAME ADDRTYPE ADDR REST; do [ "$ADDRTYPE" == "inet" ] && echo $ADDR; done)
 
-echo $IP_NEW | awk -F'/' '{print $1}' > .config.ip
-echo $(<.config.ip)
+echo $IP_NEW | awk -F'/' '{print $1}'
