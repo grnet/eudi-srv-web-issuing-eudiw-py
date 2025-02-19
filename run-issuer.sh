1a6e1f4d604a6a07663761c7993ab021e1b321f3
George Fourtounis <gfour@admin.grnet.gr>
2025-02-19 10:18:02 +0200

    Add scripts for IP-based deployment

---
+++ run-issuer.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+HOST_IP=$(<.config.ip)
+
+source .venv/bin/activate
+export REQUESTS_CA_BUNDLE=$(realpath cert.pem)
+export SERVICE_URL="https://${HOST_IP}:5000/"
+export EIDAS_NODE_URL="https://TODO1/"
+export DYNAMIC_PRESENTATION_URL="https://TODO2/"
+
+flask --app app run --cert=cert.pem --key=key.pem --host="$HOST_IP"
