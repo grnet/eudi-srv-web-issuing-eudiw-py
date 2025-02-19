c8981c20e1a02091117871a9b49bce47e5d20ca7
George Fourtounis <gfour@admin.grnet.gr>
2025-02-19 18:11:09 +0200

    Make hostname configuration repeatable

--- setup-issuer.sh
+++ setup-issuer.sh
@@ -5,4 +5,5 @@
 cp app/app_config/__config_secrets.py app/app_config/config_secrets.py
 
 IP=$(cat .config.ip)
+git restore app/metadata_config/metadata_config.json app/metadata_config/oauth-authorization-server.json app/metadata_config/openid-configuration.json
 git grep issuer.eudiw.dev | fgrep --color=none .json | cut -d ':' -f 1 | sort -u | xargs sed -i -e "s/https:\/\/issuer.eudiw.dev/https:\/\/${IP}:5000/g"
