ff0efbe31336c98a050b307a817d54c99a4b5e99
George Fourtounis <gfour@admin.grnet.gr>
2025-03-21 14:13:36 +0200

    Allow use of hostname instead of local IP

--- setup-issuer.sh
+++ setup-issuer.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
 if [ "$1" == "-h" ]; then
-    echo "Usage: setup-issuer.sh [IP]"
+    echo "Usage: setup-issuer.sh [IP] [HOSTNAME]"
     echo
-    echo "IP      the local IP of the issuer, empty for autodetection"
+    echo "IP         the local IP of the issuer, empty for autodetection"
+    echo "HOSTNAME   the hostname of the issuer (optional)"
     exit
 fi
 
 ./scripts/setup-venv.sh
-./scripts/setup-cert.sh $1
+./scripts/setup-cert.sh $1 $2
 cp app/app_config/__config_secrets.py app/app_config/config_secrets.py
 
-./scripts/setup-issuer-metadata.sh $1
+if [ "$2" != "" ]; then
+    ISSUER_LOCATION=$2
+else
+    ISSUER_LOCATION=$1
+fi
+./scripts/setup-issuer-metadata.sh ${ISSUER_LOCATION}
