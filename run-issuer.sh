633d92f8deeed4857f96cfd6302252ca19c80f35
George Fourtounis <gfour@admin.grnet.gr>
2025-03-21 14:13:36 +0200

    Allow use of hostname instead of local IP

--- run-issuer.sh
+++ run-issuer.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-HOST_IP=$(<.config.ip)
+if [ -f ".config.hostname" ]; then
+    HOST=$(<.config.hostname)
+elif [ -f ".config.ip" ]; then
+    HOST=$(<.config.ip)
+else
+    echo "Missing server setup, run setup_issuer.sh"
+    exit
+fi
 
 source .venv/bin/activate
 export REQUESTS_CA_BUNDLE=$(realpath iaca.pem)
-export SERVICE_URL="https://${HOST_IP}:5000/"
+export SERVICE_URL="https://${HOST}:5000/"
 export EIDAS_NODE_URL="https://TODO1/"
 export DYNAMIC_PRESENTATION_URL="https://TODO2/"
 
-flask --app app run --cert=cert.pem --key=key.pem --host="$HOST_IP"
+flask --app app run --cert=cert.pem --key=key.pem --host="$HOST"
