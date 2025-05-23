51645409775c4f68e02124eccbedd8dec22e8ac0
George Fourtounis <gfour@admin.grnet.gr>
2025-05-23 11:47:41 +0000

    Set up issuer (1.0) at https://snf-74864.ok-kno.grnetcloud.net:5600

--- run-issuer.sh
+++ run-issuer.sh
@@ -11,9 +11,14 @@ fi
 
 source .venv/bin/activate
 export REQUESTS_CA_BUNDLE=$(realpath iaca.pem)
-export SERVICE_URL="https://${HOST}:5000/"
+export SERVICE_URL="https://${HOST}:5600/"
 export EIDAS_NODE_URL="https://TODO1/"
 export DYNAMIC_PRESENTATION_URL="https://TODO2/"
+export FLASK_RUN_PORT=5600
+export NONCE_KEY=$(realpath private_nonce_key.pem)
+export DEFAULT_FRONTEND_URL=https://snf-74864.ok-kno.grnetcloud.net:5602
+export VERIFY_USER_ENDPOINT=https://snf-74864.ok-kno.grnetcloud.net:5601/verify/user
+export AUTH_SERVER_INTERNAL_URL=https://snf-74864.ok-kno.grnetcloud.net:5601
 
 echo "Running in branch: "$(git rev-parse --abbrev-ref HEAD)
-flask --app app run --cert=cert.pem --key=key.pem --host="$HOST"
+flask --app app run --cert=cert.pem --key=key.pem --host="$HOST" --port ${FLASK_RUN_PORT}
