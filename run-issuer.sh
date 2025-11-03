87276504f018d278d0e52d748975b0a8727e177c
George Fourtounis <gfour@admin.grnet.gr>
2025-11-03 13:35:48 +0000

    Use Let's Encrypt certificate

--- run-issuer.sh
+++ run-issuer.sh
@@ -2,8 +2,10 @@
 
 if [ -f ".config.hostname" ]; then
     HOST=$(<.config.hostname)
+    TLS="--cert=/etc/letsencrypt/live/${HOST}/fullchain.pem --key=/etc/letsencrypt/live/${HOST}/privkey.pem"
 elif [ -f ".config.ip" ]; then
     HOST=$(<.config.ip)
+    TLS=
 else
     echo "Missing server setup, run setup_issuer.sh"
     exit
@@ -16,9 +18,9 @@ export EIDAS_NODE_URL="https://TODO1/"
 export DYNAMIC_PRESENTATION_URL="https://TODO2/"
 export FLASK_RUN_PORT=5600
 export NONCE_KEY=$(realpath private_nonce_key.pem)
-export DEFAULT_FRONTEND_URL=https://snf-74864.ok-kno.grnetcloud.net:5602
-export VERIFY_USER_ENDPOINT=https://snf-74864.ok-kno.grnetcloud.net:5601/verify/user
-export AUTH_SERVER_INTERNAL_URL=https://snf-74864.ok-kno.grnetcloud.net:5601
+export DEFAULT_FRONTEND_URL=https://${HOST}:5602
+export VERIFY_USER_ENDPOINT=https://${HOST}:5601/verify/user
+export AUTH_SERVER_INTERNAL_URL=https://${HOST}:5601
 
 echo "Running in branch: "$(git rev-parse --abbrev-ref HEAD)
-flask --app app run --cert=cert.pem --key=key.pem --host="$HOST" --port ${FLASK_RUN_PORT}
+flask --app app run ${TLS} --host="$HOST" --port ${FLASK_RUN_PORT}
