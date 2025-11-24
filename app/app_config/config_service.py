f06fe34649e2a54c3c72e23cbb386b62539ecd8f
George Fourtounis <gfour@admin.grnet.gr>
2025-11-24 09:36:24 +0000

    Add MyAcademicID credential type

--- app/app_config/config_service.py
+++ app/app_config/config_service.py
@@ -142,6 +142,7 @@ class ConfService:
             "eu.europa.ec.eudi.msisdn_mdoc",
             "eu.europa.ec.eudi.educational_id_mdoc",
             "eu.europa.ec.eudi.alliance_id_mdoc",
+            "eu.europa.ec.eudi.my_academic_id_mdoc",
         ],
         "country_selection": [
             "eu.europa.ec.eudi.loyalty_mdoc",
@@ -178,6 +179,7 @@ class ConfService:
             "eu.europa.ec.eudi.educational_id_mdoc",
             "eu.europa.ec.eudi.educational_id_mdoc",
             "eu.europa.ec.eudi.alliance_id_mdoc",
+            "eu.europa.ec.eudi.my_academic_id_mdoc",
         ],
     }
 
